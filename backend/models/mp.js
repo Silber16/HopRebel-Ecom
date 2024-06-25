f62b3bbb7ea2ca90a64a27c74bcab82bf1575e79
Silber16 <fransilber16@gmail.com>
2024-06-25 10:52:56 -0300

    Cambios en .env y actualizacion de dependencias

--- backend/models/mp.js
+++ backend/models/mp.js
@@ -1,7 +1,7 @@
 
 import { MercadoPagoConfig, Preference } from 'mercadopago';
 
-const client = new MercadoPagoConfig({ accessToken: 'APP_USR-7719627064516408-031710-98268c77b535fb6f4c0a5f99f35ec741-1725842309'});
+const client = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN});
 
 const preference = new Preference(client);
 
